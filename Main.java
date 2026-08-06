b252c1b9877cfb43daebdee0e12de814555cf6f2
nik8830 <168482527+nik8830@users.noreply.github.com>
2026-08-06 18:45:04 -0300

    Completando o código.

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        //Instancia dois personagens
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
-
-        .
-        .
-        .
-
+        Personagem heroi2 = new Personagem("Adora", "Mago", 1, 70, 15);
+        //Chama o método para exibir as informações do personagem
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
     }
 }
