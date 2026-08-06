4ceeec66d1860069f33a683d05ed8fb6ad6e1334
nik8830 <168482527+nik8830@users.noreply.github.com>
2026-08-06 18:51:21 -0300

    Completando o código
    Adicionado o construtor do personagem.
    Finalizado o método para exibir as informações dos personagens.

--- Personagem.java
+++ Personagem.java
@@ -1,3 +1,4 @@
+//cria a classe pública Personagem
 public class Personagem {
     String nome;
     String classe;
@@ -5,20 +6,23 @@ public class Personagem {
     int pontosDeVida;
     double poderBase;
 
+//Construtor do Personagem
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
+//Método para exibir as informações do personagem
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.println("Nome:" + nome);
+        System.out.println("Classe:" + classe);
+        System.out.println("Nivel:" + nivel);
+        System.out.println("Vida:" + pontosDeVida);
+        System.out.println("Poder:" + poderBase);
 
     }
 }
-
